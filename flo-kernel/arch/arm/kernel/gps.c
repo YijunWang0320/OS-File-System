39695501735e71f86809fbd6c20d4cb567215410
xw2353 <xw2353@columbia.edu>
2014-11-30 15:46:33 -0500

    add daemon

--- flo-kernel/arch/arm/kernel/gps.c
+++ flo-kernel/arch/arm/kernel/gps.c
@@ -3,6 +3,7 @@
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 struct gps_location *local_kernel;
+
 SYSCALL_DEFINE(set_gps_location) (struct gps_location *loc) {
 	struct gps_location *tmp_loc_kernel = (struct gps_location *)kmalloc(sizeof(struct gps_location), GFP_KERNEL);
 	int ret = copy_from_user(&tmp_loc_kernel, loc, sizeof(struct gps_location));
@@ -10,8 +11,11 @@ SYSCALL_DEFINE(set_gps_location) (struct gps_location *loc) {
 		return -1;
 
 	local_kernel->latitude = tmp_loc_kernel->latitude;
+	printk("local_kernel->latitude: %f \n", local_kernel->latitude);
 	local_kernel->longitude = tmp_loc_kernel->longitude;
+	printk("local_kernel->longitude: %f \n", local_kernel->longitude);
 	local_kernel->accuracy = tmp_loc_kernel->accuracy;
+	printk("local_kernel->accuracy: %f \n", local_kernel->accuracy);
 
 
 	return 10;
