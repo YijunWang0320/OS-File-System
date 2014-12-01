5ecd4fa033314a9dbd15a81b4dcee262e7011af9
xw2353 <xw2353@columbia.edu>
2014-11-30 19:21:23 -0500

    move syscall to kernel

--- flo-kernel/include/linux/syscalls.h
+++ flo-kernel/include/linux/syscalls.h
@@ -858,6 +858,5 @@ asmlinkage long sys_process_vm_writev(pid_t pid,
 				      const struct iovec __user *rvec,
 				      unsigned long riovcnt,
 				      unsigned long flags);
-asmlinkage long sys_set_gps_location(struct gps_location __user *loc);
 
 #endif
