b8feac270b0d12cad57e00634b0ce5329f9847b2
Wei Dai <wdai2248@columbia.edu>
2014-12-02 19:01:18 -0500

    for clean

--- flo-kernel/kernel/gps.c
+++ flo-kernel/kernel/gps.c
@@ -2,6 +2,8 @@
 #include <linux/unistd.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
+#include <linux/limits.h>
+
 struct gps_location *local_kernel;
 
 static void __init init_local_kernel(void)
@@ -11,24 +13,24 @@ static void __init init_local_kernel(void)
 
 SYSCALL_DEFINE(set_gps_location) (struct gps_location *loc) {
 	struct gps_location *tmp_loc_kernel = (struct gps_location *)kmalloc(sizeof(struct gps_location), GFP_KERNEL);
-	int ret = copy_from_user(local_kernel, loc, sizeof(struct gps_location));
+	int ret = copy_from_user(tmp_loc_kernel, loc, sizeof(struct gps_location));
 	if(ret != 0)
-		return -1;
-	
-	//printk("tmp_loc_kernel: %lu\n", tmp_loc_kernel->latitude);
-	//local_kernel->latitude = tmp_loc_kernel->latitude;
-	printk("local_kernel->latitude: %d \n", local_kernel->latitude);
-	//printk("tmp_loc_kernel longitude: %lu \n", tmp_loc_kernel->longitude);
-	//local_kernel->longitude = tmp_loc_kernel->longitude;
-	printk("local_kernel->longitude: %d \n", local_kernel->longitude);
-	//local_kernel->accuracy = tmp_loc_kernel->accuracy;
-
-
-	return 10;
+		return -EFAULT;
+	return 0;
 }
+
+
 
 SYSCALL_DEFINE2(get_gps_location, const char __user *, pathname,
 	struct gps_location __user *, loc) {
+	if (pathname == NULL || loc == NULL)
+		return -EINVAL;
+
+	struct gps_location loc_k;
+	char *pathname_k;
+
+	pathname_k = kmalloc(PATH_MAX+1, sizeof(char), GFP_KERNEL);
+
 	return 0;
 }
 
