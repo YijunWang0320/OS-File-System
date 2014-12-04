d6200b42654e12cf1498c5354f25a4c83cdc45e3
xw2353 <xw2353@columbia.edu>
2014-12-03 19:26:35 -0500

    update file_loc

--- userspace/file_loc/file_loc.c
+++ userspace/file_loc/file_loc.c
@@ -1,6 +1,15 @@
 #include "file_loc.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 int main(int argc, char **argv)
 {
+	const char *pathname = "/data/misc/hmwk6/test.txt";
+	struct gps_location *loc = (gps_location *)malloc(sizeof(struct gps_location));
+	int ret = get_gps_location(pathname, loc);
+	printf("latitude : %f \n", loc->latitude);
+	printf("longitude: %f \n", loc->longitude);
+	printf("accuracy: %f \n", loc->accuracy);
+	printf("timestamp: %d \n", ret);
 	return 0;
 }
