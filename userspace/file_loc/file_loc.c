e600ae0b8e34270b21b7570b2dd7699c04b032ed
WangYijun <yw2676@columbia.edu>
2014-12-04 22:18:25 -0500

    checkpatch except kernel

--- userspace/file_loc/file_loc.c
+++ userspace/file_loc/file_loc.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <error.h>
 #include "file_loc.h"
 
 int main(int argc, char **argv)
@@ -8,27 +9,29 @@ int main(int argc, char **argv)
 	char *pathname;
 	if (argc != 2) {
 		printf("Wrong parameter!\n");
-		return -1;
+		return -EINVAL;
 	}
 
 	pathname = argv[1];
 
-	struct gps_location *loc = (struct gps_location *)malloc(sizeof(struct gps_location));
+	struct gps_location *loc = malloc(sizeof(struct gps_location));
 	int ret = get_gps_location(pathname, loc);
 	if (ret < 0) {
 			free(loc);
-			printf("Fail to get location information of the given path name!\n");
-			return -1;
+			printf("Fail to get location information
+			 of the given path name!\n");
+			return -EINVAL;
 	}
 	printf("GPS information:\n");
-	printf("latitude : %f \n", loc->latitude);
-	printf("longitude: %f \n", loc->longitude);
-	printf("accuracy: %f \n", loc->accuracy);
-	printf("Data age: %d s \n", ret);
+	printf("latitude : %f\n", loc->latitude);
+	printf("longitude: %f\n", loc->longitude);
+	printf("accuracy: %f\n", loc->accuracy);
+	printf("Data age: %d s\n", ret);
 	printf("Map URL:\n");
 	printf("https://maps.googleapis.com/maps/api/staticmap?
 		zoom=13&size=600x600&maptype=roadmap
-		&markers=color:red|label:F|%f,%f",loc->latitude,loc->longitude);
+		&markers=color:red|label:F|%f,%f",
+		loc->latitude, loc->longitude);
 	free(loc);
 	return 0;
 }
